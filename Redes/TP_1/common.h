8800e55fb34dc66be4019d02ef719436399a612d
VictorHSMoura <victorhugosmoura@gmail.com>
2021-01-11 10:24:50 -0300

    [Redes] Add initial base code

---
+++ Redes/TP_1/common.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <stdlib.h>
+#include <arpa/inet.h>
+
+void logexit(const char *msg);
+int addrparse(const char *addrstr, const char *portstr, struct sockaddr_storage *storage);
+void addrtostr(const struct sockaddr *addr, char *str, ssize_t strsize);
+int server_sockaddr_init(const char *proto, const char *portstr, struct sockaddr_storage *storage);
