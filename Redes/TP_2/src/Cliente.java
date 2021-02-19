4d3d25c1ba39f58b30608e9be2e9dcdd2c7fe603
VictorHSMoura <victorhugosmoura@gmail.com>
2021-02-19 20:22:34 -0300

    [Redes] Add UDP communication. Need to fix it

--- Redes/TP_2/src/Cliente.java
+++ Redes/TP_2/src/Cliente.java
@@ -3,13 +3,14 @@ import java.net.*;
 import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
+import java.util.Vector;
 
 public class Cliente {
     public final static int SERVICE_PORT = 51551;
 
     public static void main(String[] args) {
         if (args.length != 3) {
-            System.err.println("Quantidade de argumentos incorreta");
+            System.err.println("Incorrect number of arguments");
             return;
         }
         String serverIP = args[0];
@@ -19,43 +20,53 @@ public class Cliente {
         try {
             port = Integer.parseInt(args[1]);
         } catch (NumberFormatException e) {
-            System.err.println("A porta de conexão deve conter um valor inteiro.");
+            System.err.println("The port number needs to be a integer");
             return;
         }
 
         try {
             InetAddress ip = InetAddress.getByName(serverIP);
             Socket clientSocket = new Socket(ip, port);
+
             DataInputStream is = new DataInputStream(clientSocket.getInputStream());
             DataOutputStream out = new DataOutputStream(clientSocket.getOutputStream());
+
             byte[] messageID;
             byte[] header;
             byte[] receivedData = new byte[1024];
+
             int receivedMessageID = 0;
             int sentMessageID;
-            int messageSize = 0;
-            int UDPPort;
+            int UDPPort = 54321;    // porta default
+
+            DatagramSocket udpSocket = new DatagramSocket();
+            File file = new File(fileName);
+            InputStream fileInput = new FileInputStream(file);
+            int fileLength = (int) file.length();
+            byte[] fileByteArray = new byte[fileLength];
+            fileInput.read(fileByteArray);
 
             header = ByteBuffer.allocate(2).putShort((short) 1).array();
+            System.out.println("Sending hello message");
 
             while (receivedMessageID != 4) {
                 out.write(header);
 
-                messageSize = is.read(receivedData, 0, receivedData.length);
+                int messageSize = is.read(receivedData, 0, receivedData.length);
                 receivedMessageID = (int) ByteBuffer.wrap(Arrays.copyOfRange(receivedData, 0, 2)).getShort();
-                System.out.println("Message size: " + messageSize);
 
                 switch (receivedMessageID) {
                     case 2:
                         UDPPort = ByteBuffer.wrap(Arrays.copyOfRange(receivedData, 2, 6)).getInt();
-                        System.out.println("message: " + receivedMessageID + ", " + UDPPort + " - tamanho: " + messageSize);
+                        System.out.println("Transmission port: " + UDPPort);
 
+                        // sending file data
                         sentMessageID = 3;
                         messageID = ByteBuffer.allocate(2).putShort((short) sentMessageID).array();
                         byte[] filenameInBytes = new byte[15];
                         System.arraycopy(fileName.getBytes(), 0, filenameInBytes, 0, fileName.getBytes().length);
-                        byte[] fileSize = ByteBuffer.allocate(8).putLong((long) 1e10).array();
-
+                        byte[] fileSize = ByteBuffer.allocate(8).putLong(fileLength).array();
+                        System.out.println("Sending file info");
 
                         header = new byte[messageID.length + filenameInBytes.length + fileSize.length];
 
@@ -65,45 +76,65 @@ public class Cliente {
                                 messageID.length + filenameInBytes.length, fileSize.length);
                         break;
                     case 4:
-                        System.out.println("message: " + receivedMessageID);
-                        // todo: enviar o arquivo via UDP, utlizando a porta alocada
+                        System.out.println("Ready to send the file");
                         break;
                 }
             }
+
+            Thread.sleep(200); //Evita condição de corrida
+
+            int sequenceNumber = 0;
+            boolean lastMessageFlag = false;
+
+            int windowSize = 128;
+            int payloadSize = 1000;
+            Vector<byte[]> sentMessageList = new Vector<>();
+
+            for (int i=0; i < fileByteArray.length; i = i+payloadSize) {
+                if ((i+payloadSize) >= fileByteArray.length) {
+                    payloadSize = fileByteArray.length - i;
+                }
+
+                sentMessageID = 3;
+                messageID = ByteBuffer.allocate(2).putShort((short) sentMessageID).array();
+
+                byte[] sequenceNumberInBytes = ByteBuffer.allocate(4).putInt(sequenceNumber).array();
+                // Create new byte array for message
+                byte[] message = new byte[1024];
+
+                byte[] payloadSizeInBytes = ByteBuffer.allocate(2).putShort((short) payloadSize).array();
+
+                header = new byte[messageID.length + sequenceNumberInBytes.length + payloadSizeInBytes.length + payloadSize];
+
+                System.arraycopy(messageID, 0, header, 0, messageID.length);
+                System.arraycopy(sequenceNumberInBytes, 0, header, messageID.length, sequenceNumberInBytes.length);
+                System.arraycopy(payloadSizeInBytes, 0, header,
+                        (messageID.length + sequenceNumberInBytes.length), payloadSizeInBytes.length);
+                System.arraycopy(fileByteArray, i, header,
+                        (messageID.length + sequenceNumberInBytes.length + payloadSizeInBytes.length),
+                        payloadSize);
+
+                // Package the message
+                DatagramPacket sendPacket = new DatagramPacket(header, header.length, ip, UDPPort);
+                udpSocket.send(sendPacket);
+                System.out.println("Sent: Sequence number = " + sequenceNumber);
+
+                // Add the message to the sent message list
+                sentMessageList.add(message);
+                sequenceNumber += 1;
+            }
+
+            // closing resources
+            is.close();
+            out.close();
+
+
+            System.out.println("Closing this connection : " + clientSocket);
             clientSocket.close();
-        } catch (IOException e) {
+            System.out.println("Connection closed");
+
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
-
-//    UDP PART
-//    public static void main(String[] args) {
-//        try{
-//            DatagramSocket clientSocket = new DatagramSocket();
-//
-//            InetAddress IPAddress = InetAddress.getByName("localhost");
-//
-//            byte[] sendingDataBuffer = new byte[1024];
-//            byte[] receivingDataBuffer = new byte[1024];
-//
-//            String sentence = "Hello from UDP client";
-//            sendingDataBuffer = sentence.getBytes();
-//
-//            DatagramPacket sendingPacket = new DatagramPacket(sendingDataBuffer, sendingDataBuffer.length, IPAddress, SERVICE_PORT);
-//
-//            clientSocket.send(sendingPacket);
-//
-//            DatagramPacket receivingPacket = new DatagramPacket(receivingDataBuffer, receivingDataBuffer.length);
-//            clientSocket.receive(receivingPacket);
-//
-//            String receivedData = new String(receivingPacket.getData());
-//            System.out.println("Sent from the server: " + receivedData);
-//
-//            clientSocket.close();
-//        } catch (SocketException | UnknownHostException e) {
-//            e.printStackTrace();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
 }
--- Redes/TP_2/src/Servidor.java
+++ Redes/TP_2/src/Servidor.java
@@ -1,6 +1,4 @@
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.*;
 import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
@@ -23,22 +21,24 @@ public class Servidor {
             int receivedMessageID = 0;
             int sentMessageID;
             int messageSize = 0;
+            int clientPort = 54321; // need to change this
 
-            long fileSize;
-            String fileName;
+            long fileSize = 3;
+            String fileName = "default-file.txt";
 
             while (receivedMessageID != 3) {
                 // receiving client messages
                 messageSize = serverInput.read(receivedData, 0, receivedData.length);
-                System.out.println("Message size: " + messageSize);
 
                 receivedMessageID = (int) ByteBuffer.wrap(Arrays.copyOfRange(receivedData, 0, 2)).getShort();
 
                 switch (receivedMessageID) {
                     case 1: // hello message
-                        int port = 50001; // need to change this
-                        byte[] portInBytes = ByteBuffer.allocate(4).putInt(port).array();
+                         System.out.println("Hello message received. Allocating client port");
 
+                        byte[] portInBytes = ByteBuffer.allocate(4).putInt(clientPort).array();
+
+                        System.out.println("Port allocated: " + clientPort);
                         sentMessageID = 2;
                         messageID = ByteBuffer.allocate(2).putShort((short) sentMessageID).array();
 
@@ -50,56 +50,76 @@ public class Servidor {
                     case 3:
                         byte[] fileNameInBytes = Arrays.copyOfRange(receivedData, 2, 17);
                         fileName = new String(fileNameInBytes, StandardCharsets.UTF_8);
-                        fileSize = (long) ByteBuffer.wrap(Arrays.copyOfRange(receivedData, 17, messageSize)).getLong();
-                        System.out.println(receivedMessageID + ", " + fileName + ", " + fileSize);
+                        fileSize = ByteBuffer.wrap(Arrays.copyOfRange(receivedData, 17, messageSize)).getLong();
+                        System.out.println("File info - Name: " + fileName + " - Size (in bytes): " + fileSize);
 
-                        // todo: alocar estruturas necessárias para o arquivo
-                        // enviando mensagem ok
+                        System.out.println("Allocating memory for the file");
+
+                        // sending ok message
                         sentMessageID = 4;
                         messageID = ByteBuffer.allocate(2).putShort((short) sentMessageID).array();
                         header = messageID;
+                        System.out.println("Ready to receive file upload");
                         serverOutput.write(header);
                         break;
-                        // todo: fazer case para recebimento de dados
                 }
             }
+
+            DatagramSocket clientSocket = new DatagramSocket(clientPort);
+            File file = new File("arquivo-recebido.txt");
+            file.createNewFile();
+            FileOutputStream fileOutput = new FileOutputStream(file);
+
+            boolean lastMessageFlag = false;
+            int sequenceNumber = 0;
+            int lastSequenceNumber = -1;
+            int payloadSize = 0;
+            int packetSize = 1000;
+            int numberOfPackets = (int) (fileSize/packetSize);
+
+            if (fileSize % packetSize != 0)
+                numberOfPackets++;
+
+            while (!lastMessageFlag) {
+                byte[] message = new byte[1024];
+                byte[] fileByteArray = new byte[packetSize];
+                DatagramPacket receivedPacket = new DatagramPacket(message, message.length);
+                clientSocket.setSoTimeout(0);
+                clientSocket.receive(receivedPacket);
+
+                message = receivedPacket.getData();
+                receivedMessageID = (int) ByteBuffer.wrap(Arrays.copyOfRange(message, 0, 2)).getShort();
+                sequenceNumber = ByteBuffer.wrap(Arrays.copyOfRange(message, 2, 6)).getInt();
+                packetSize = ByteBuffer.wrap(Arrays.copyOfRange(message, 6, 8)).getShort();
+
+
+                if (sequenceNumber == numberOfPackets - 1) {
+                    lastMessageFlag = true;
+                } else {
+                    lastMessageFlag = false;
+                }
+                if (sequenceNumber == (lastSequenceNumber + 1)) {
+                    lastSequenceNumber = sequenceNumber;
+
+                    fileByteArray = Arrays.copyOfRange(message, 8, 8 + packetSize);
+
+                    fileOutput.write(fileByteArray);
+                    System.out.println("Received: Sequence number = " + sequenceNumber);
+                    if (lastMessageFlag) {
+                        fileOutput.close();
+                    }
+                }
+            }
+
+            clientSocket.close();
+            System.out.println("File received.");
+            System.out.println("Client " + clientConn + " sends exit...");
+            System.out.println("Closing connection.");
             clientConn.close();
+            System.out.println("Connection closed");
 
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-
-//    UDP Part
-//    public static void main(String[] args) throws IOException {
-//        try {
-//            DatagramSocket serverSocket = new DatagramSocket(SERVICE_PORT);
-//
-//            byte[] receivingDataBuffer = new byte[1024];
-//            byte[] sendingDataBuffer = new byte[1024];
-//
-//            DatagramPacket inputPacket = new DatagramPacket(receivingDataBuffer, receivingDataBuffer.length);
-//            System.out.println("Waiting for a client to connect...");
-//
-//            serverSocket.receive(inputPacket);
-//
-//            String receivedData = new String(inputPacket.getData());
-//            System.out.println("Sent from the client: " + receivedData);
-//
-//            sendingDataBuffer = receivedData.toUpperCase().getBytes();
-//
-//            InetAddress senderAddress = inputPacket.getAddress();
-//            int senderPort = inputPacket.getPort();
-//
-//            DatagramPacket outputPacket = new DatagramPacket(
-//                    sendingDataBuffer, sendingDataBuffer.length,
-//                    senderAddress, senderPort
-//            );
-//
-//            serverSocket.send(outputPacket);
-//            serverSocket.close();
-//        } catch (SocketException e) {
-//            e.printStackTrace();
-//        }
-//    }
 }
