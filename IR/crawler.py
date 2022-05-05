c4b41372e3d6a6d4d227b4d7a8119c36f3812bc3
Victor Hugo Moura <victorhugosmoura@gmail.com>
2022-05-05 11:32:18 -0300

    Update crawler.py

--- IR/crawler.py
+++ IR/crawler.py
@@ -45,76 +45,67 @@ class Crawler(Thread):
 
 
         while (count < 1000):
-            url = None
-            page = None
-
-            queueLock.acquire()
-            if (len(queue) > 0):
-                url = queue.pop(0)
-                page = WebPage(url)
-            queueLock.release()
-
             try:
-                secondaryLock.acquire()
-                checkPageVisited = url is not None and page.pageURL not in visited
-                secondaryLock.release()
+                url = None
+                page = None
 
-                if (checkPageVisited):
-                    if(page.crawlable() and page.isHTML()):
-                        secondaryLock.acquire()
-                        ct = datetime.now()
-                        checkDelay = self.isOnDelay(page=page, hostDelays=hostDelays, ct=ct)
-                        secondaryLock.release()
+                queueLock.acquire()
+                if (len(queue) > 0):
+                    url = queue.pop(0)
+                    page = WebPage(url)
+                queueLock.release()
 
-                        if (checkDelay):
-                            page.crawlPage()
-
-                            if (page.pageRequest is not None):
-                                
-                                secondaryLock.acquire()
-                                # check again if page was visited
-                                if page.pageURL in visited:
-                                    secondaryLock.release()
-                                    continue
-
-                                # add page to visited pages and store visit time
-                                ct = datetime.now()
-                                visited.add(page.pageURL)
-                                hostDelays[page.host] = ct.timestamp()
-                                secondaryLock.release()
-
-                                links = page.getLinks()
-                                
-                                queueLock.acquire()
-                                for link in links:
-                                    if link not in visited:
-                                        queue.append(link)
-                                queueLock.release()
-
-                                writeLock.acquire()
-                                if count < 1000:
-                                    print(page.pageURL)
-                                    if (count > 0 and count % 100 == 0):
-                                        print("Changing File")
-                                        outFile.close()
-                                        outFile = open(f'corpus/crawl{int(count/100)}.warc.gz', 'wb')
-                                        writer = WARCWriter(outFile, gzip=True)
-                                    
-                                    self.writePageToFile(page=page, warcWriter=writer)
-                                    count+=1
-                                writeLock.release()
-                                
-                        else:
-                            queueLock.acquire()
-                            queue.append(url)
-                            queueLock.release()
-            except:
-                if secondaryLock.locked():
+                
+                if(url is not None and page.crawlable() and page.isHTML()):
+                    secondaryLock.acquire()
+                    ct = datetime.now()
+                    checkDelay = self.isOnDelay(page=page, hostDelays=hostDelays, ct=ct)
                     secondaryLock.release()
+
+                    if (checkDelay):
+                        page.crawlPage()
+
+                        if (page.pageRequest is not None):
+                            secondaryLock.acquire()
+                            # store visit time
+                            ct = datetime.now()
+                            hostDelays[page.host] = ct.timestamp()
+                            secondaryLock.release()
+
+                            links = page.getLinks()
+                            
+                            queueLock.acquire()
+                            for link in links:
+                                # add page to visited to prevent adding duplicate pages
+                                if link not in visited:
+                                    queue.append(link)
+                                    visited.add(link)
+                            queueLock.release()
+
+                            writeLock.acquire()
+                            if count < 1000:
+                                print(page.pageURL)
+                                if (count > 0 and count % 100 == 0):
+                                    print("Changing File")
+                                    outFile.close()
+                                    outFile = open(f'corpus/crawl{int(count/100)}.warc.gz', 'wb')
+                                    writer = WARCWriter(outFile, gzip=True)
+                                
+                                self.writePageToFile(page=page, warcWriter=writer)
+                                count+=1
+                            writeLock.release()
+                            
+                    else:
+                        queueLock.acquire()
+                        queue.append(url)
+                        queueLock.release()
+            except:
+                if queue.locked():
+                    queue.release()
 
 count = 0
 queue = ["https://g1.globo.com"]
-visited = set()
+visited = set("https://g1.globo.com")
 hostDelays = {}
 
 queueLock = Lock()
